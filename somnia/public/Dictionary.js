55387e07be1a059482e114f699f268e20ce0e8b9
Przemysław Klejno <pemix09@onet.pl>
2021-10-21 17:20:56 +0200

    dodanie paska wyszukiwania

---
+++ somnia/public/Dictionary.js
@@ -0,0 +1,22 @@
+function GetTranslation(component, language){
+    switch(language.toUpper()){
+        case 'EN':
+            switch(component.toUpper()){
+                case 'HEADER':
+                    return {
+                        
+                    }
+
+            }
+            break;
+
+        case 'HU':
+            switch(component.toUpper()){
+
+            }
+            break;
+
+        default:
+            return null;
+    }
+}
--- somnia/src/components/Header/Search/Search.js
+++ somnia/src/components/Header/Search/Search.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React,{useEffect} from "react";
 import './Search.css';
 
+
+
+
 function Search(props){
+
+    const [ActiveSearch, SetActiveSearch] = React.useState(false);
+
+    //useEffect is envoked every time we update the component. Also when it mounts.
+    useEffect(() => {
+        
+        const SearchInput = document.getElementById('SearchInput');
+        SearchInput.classList.toggle('Hidden');
+
+    })
+
     return(
-        <svg class='SearchIcon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+        <div className='RowFlex'>
+            <svg class='SearchIcon' id='SearchIcon' onClick={ () => SetActiveSearch(true)} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z"/></svg>
+            <input class='SearchInput' id='SearchInput' type='text'></input>
+        </div>
     )
 }
+
+
 
 export default Search;
