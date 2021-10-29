ff3759a96f9429a986aa0b6d3382f081f691db5e
Przemysław Klejno <pemix09@onet.pl>
2021-10-29 13:19:54 +0200

    login starts to work

--- somnia/src/App.js
+++ somnia/src/App.js
@@ -1,42 +1,35 @@
 import React, { useEffect, useState } from "react";
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
-import Footer from './components/Footer/Footer';
+import Footer from "./components/Footer/Footer";
 
 function App() {
-
   //here we have the state of our React function
-  const [lan, setLan] = useState('EN');
+  const [lan, setLan] = useState("EN");
   const [search, setSearch] = useState(true);
   const [login, SetLogin] = useState(false);
-  const [ActiveContent, SetActiveContent] = useState('MENU');
+  const [ActiveContent, SetActiveContent] = useState("MENU");
 
   //it's simillar to ComponentDidMount, so it's envoked every
   //time state hanges
   useEffect(() => {
-    document.title = 'Somnia';
-    console.log(ActiveContent);
-  })
-
+    document.title = "Somnia";
+  });
 
   //the function for changing React function state
   function handleChange(newStateValue) {
     switch (newStateValue.type.toUpperCase()) {
-      case 'LANCHANGE':
-        {
-          setLan(newStateValue.value);
-        }
-
-      case 'LOGIN':
-        {
-          SetLogin(newStateValue.value);
-          SetActiveContent('LOGIN');
-        }
+      case "LANCHANGE":
+        setLan(newStateValue.value);
+        break;
+      case "LOGIN":
+        SetLogin(newStateValue.value);
+        SetActiveContent("LOGIN");
+        break;
       default:
-        {
-          SetActiveContent('MENU');
-          SetLogin(false);
-        }
+        SetActiveContent("MENU");
+        SetLogin(false);
+        break;
     }
   }
 
--- somnia/src/components/Content/Content.js
+++ somnia/src/components/Content/Content.js
@@ -1,55 +1,75 @@
-import React,{useEffect,useState} from "react";
-import './Content.css';
+import React, { useEffect, useState } from "react";
+import "./Content.css";
 import MainMenu from "./MainMenu/MainMenu";
 import Login from "./Login/Login";
 import About from "./About/About";
 import Collections from "./Collections/Collections";
 import Products from "./Products/Products";
 
+function Content(props) {
+  const [ActiveItem, SetActiveItem] = useState(
+    props.activeContent.toString().toUpperCase()
+  );
 
-function Content(props){
+  function GetActiveElement() {
+    switch (props.activeContent) {
+      case "LOGIN":
+        return <Login onChange={HandleChange}></Login>;
 
-    const [ActiveItem, SetActiveItem] = useState(props.activeContent.toString().toUpperCase());
+      case "MENU":
+        return <MainMenu onChange={HandleChange}></MainMenu>;
 
-    function GetActiveElement(){
-        switch(ActiveItem){
-            case 'LOGIN': return <Login onChange={HandleChange}></Login>;
-            case 'MENU' : return <MainMenu onChange={HandleChange}></MainMenu>;
-            case 'ABOUT' : return<About  onChange={HandleChange}></About>;
-            case 'COLLECTIONS': return <Collections onChange={HandleChange}></Collections>;
-            case 'PRODUCTS':return<Products onChange={HandleChange}></Products>;
-            
-        }
+      case "ABOUT":
+        return <About onChange={HandleChange}></About>;
+
+      case "COLLECTIONS":
+        return <Collections onChange={HandleChange}></Collections>;
+
+      case "PRODUCTS":
+        return <Products onChange={HandleChange}></Products>;
+
+      default:
+        console.log("default");
+        break;
     }
+  }
 
-    function HandleChange(newStateValue)
-    {
-        switch(newStateValue.type.toString().toUpperCase()){
-            case 'MENUITEMCHOOSEN':{
-                switch(newStateValue.value.toString().toUpperCase()){
-                    case 'ABOUT': SetActiveItem(newStateValue.value.toString().toUpperCase());
-                    case 'COLLECTIONS': SetActiveItem(newStateValue.value.toString().toUpperCase());
-                    case 'PRODUCTS' : SetActiveItem(newStateValue.value.toString().toUpperCase());
-                }
-            }
-            case 'GETBACK':{
-                switch(newStateValue.value.toString().toUpperCase()){
-                    case 'MENU': SetActiveItem(newStateValue.value.toString().toUpperCase());
-                }
-
-            }
+  function HandleChange(newStateValue) {
+    switch (newStateValue.type.toString().toUpperCase()) {
+      case "MENUITEMCHOOSEN":
+        switch (newStateValue.value.toString().toUpperCase()) {
+          case "ABOUT":
+            SetActiveItem(newStateValue.value.toString().toUpperCase());
+            break;
+          case "COLLECTIONS":
+            SetActiveItem(newStateValue.value.toString().toUpperCase());
+            break;
+          case "PRODUCTS":
+            SetActiveItem(newStateValue.value.toString().toUpperCase());
+            break;
+          default:
+            return;
         }
+        break;
+      case "GETBACK":
+        switch (newStateValue.value.toString().toUpperCase()) {
+          case "MENU":
+            SetActiveItem(newStateValue.value.toString().toUpperCase());
+            break;
+          default:
+            return;
+        }
+        break;
+      default:
+        return;
     }
-    let ActiveContent = GetActiveElement();
-    useEffect(()=>{
-       
-    })
+  }
+  let ActiveContent = GetActiveElement();
 
-    return(
-        <div class='Content'>
-            {ActiveContent}
-        </div>
-    )
+  useEffect(() => {
+  });
+
+  return <div class="Content">{ActiveContent}</div>;
 }
 
 export default Content;
