75b276cdad317460e8816e289de847a405a2979d
Przemysław Klejno <pemix09@onet.pl>
2021-10-30 20:06:10 +0200

    added context for active tab, but not implemented

---
+++ somnia/src/globalContext/context/ActiveContentContext.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react';
+const ActiveContentContext = React.createContext();
+
+export function ActiveContentProvider(props){
+    const [activeContent,SetActiveContent] = useState('MENU');
+
+    function ToggleActiveContext(newActiveContent)
+    {
+        SetActiveContent(newActiveContent);
+    }
+    let ActiveContentObject = {value:'MENU',HandleChange:ToggleActiveContext}
+
+    return(
+        <ActiveContentContext.Provider value={ActiveContentObject}>
+            {props.children}
+        </ActiveContentContext.Provider>
+    )
+}
+
+export const UseActiveContentContext = () => React.useContext(ActiveContentContext);
--- somnia/src/public/Dictionary.js
+++ somnia/src/public/Dictionary.js
@@ -49,6 +49,32 @@ export function GetTranslation(component, language){
                             collections: 'KOLLEKTÁCIÓK'
                         }
                     case 'SEARCHBAR': return{placeHolder:'Keress valamit itt...'};
+                    case 'LOGIN': 
+                    return {
+                    emailLogin:'email or login',
+                    emailPlaceHolder: 'email address',
+                    password: 'password',
+                    noAccount: "Don't have account yet? Sign up here...",
+                    cancel: 'cancel',
+                    login: 'login'
+                }
+                case 'REGISTER':
+                    return{
+                    emailLogin:'email or login',
+                    email: 'email address',
+                    password: 'password',
+                    passwordAgain: 'password again',
+                    cancel: 'cancel',
+                    register: 'register'
+                    }
+                case 'FOOTER':
+                    return{
+                    contact:'Contact us',
+                    terms: 'Terms and conditions',
+                    privacy: 'Privacy Policy',
+                    shipRet: 'Shipping&Returns',
+                    faq: 'FAQ'
+                    }
                     default: return;
     
                 }
