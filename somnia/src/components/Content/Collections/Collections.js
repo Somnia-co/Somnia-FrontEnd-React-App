b89ec1068c59ef854f2b0cc8d4a82200adb1109b
Przemysław Klejno <pemix09@onet.pl>
2021-11-06 13:42:56 +0100

    Added working collections buttons

--- somnia/src/components/Content/Collections/Collections.js
+++ somnia/src/components/Content/Collections/Collections.js
@@ -4,11 +4,13 @@ import { GetCollections } from "../../../public/CollectionsList";
 import CollectionButton from "./collection/CollectionButton.js";
 import { GetTranslation } from "../../../public/Dictionary.js";
 import { UseLanguageContext } from "../../../globalContext/context/LanguageContext";
+import {UseActiveContentContext} from '../../../globalContext/context/ActiveContentContext';
 import ContentTitle from "../ContentTitle/ContentTitle";
 
 export default function Collections(props) {
   const component = "Collections";
   const globalLanguage = UseLanguageContext();
+  const ActiveContent = UseActiveContentContext();
 
   let collections = GetCollections();
   let translation = GetTranslation(component, globalLanguage.value);
@@ -21,7 +23,7 @@ export default function Collections(props) {
       ></ContentTitle>
       <div className='Collections'>
         {collections.map((collection) => {
-          return <CollectionButton>{collection}</CollectionButton>;
+          return <CollectionButton onClick={() => ActiveContent.HandleChange('SingleCollection',collection.name)}>{collection}</CollectionButton>;
         })}
       </div>
     </div>
