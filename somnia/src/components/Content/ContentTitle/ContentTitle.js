90388be3baacf87975734e52627d983179431efd
Przemysław Klejno <pemix09@onet.pl>
2021-11-16 17:40:21 +0100

    The other arrow added for going back

--- somnia/src/components/Content/ContentTitle/ContentTitle.js
+++ somnia/src/components/Content/ContentTitle/ContentTitle.js
@@ -1,22 +1,21 @@
 import React from "react";
-import './ContentTitle.css';
-import {UseActiveContentContext} from '../../../globalContext/context/ActiveContentContext'
+import "./ContentTitle.css";
+import { UseActiveContentContext } from "../../../globalContext/context/ActiveContentContext";
+import arrow from "../../../images/Arrow/back.png";
 
-export default function ContentTitle(props){
+export default function ContentTitle(props) {
+  const ActiveContext = UseActiveContentContext();
 
-    const ActiveContext = UseActiveContentContext();
+  return (
+    <div className="ContentTitleDiv">
+      <img
+        onClick={() => ActiveContext.HandleChange("MENU")}
+        className="ContentTitleIcon"
+        src={arrow}
+        alt='SomeArrow'
+      ></img>
 
-    return(
-        <div className='RowFlex'>
-        <svg
-          onClick={() => ActiveContext.HandleChange('MENU')}
-          className='ContentTitleIcon'
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3.222 18.917c5.666-5.905-.629-10.828-5.011-7.706l1.789 1.789h-6v-6l1.832 1.832c7.846-6.07 16.212 4.479 7.39 10.085z" />
-        </svg>
-        <h1 className="ContentTitle">{props.title}</h1>
-      </div>
-    )
+      <h1 className="ContentTitle">{props.title}</h1>
+    </div>
+  );
 }
