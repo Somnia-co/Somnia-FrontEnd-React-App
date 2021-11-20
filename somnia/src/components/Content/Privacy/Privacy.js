eaf484da9876c0a681d72d521c11ee1d119a95c7
pemix09 <pemix09@onet.pl>
2021-11-20 17:55:18 +0100

    finished privacy page

--- somnia/src/components/Content/Privacy/Privacy.js
+++ somnia/src/components/Content/Privacy/Privacy.js
@@ -1,17 +1,21 @@
 import React from "react";
-import './Privacy.css';
+import "./Privacy.css";
 import logo from "../../../images/Logo/Somnialogo1.png";
-import ContentTitle from '../ContentTitle/ContentTitle';
+import ContentTitle from "../ContentTitle/ContentTitle";
 
-export default function Privacy(props){
-    return(
-        <div className="PrivacyContent">
-            <ContentTitle
-                onChange={props.onChange}
-                title='Privacy Policy'
-            ></ContentTitle>
-            <img src={logo}></img>
-            <p>The content of Privacy page</p>
-        </div>
-    )
+export default function Privacy(props) {
+  return (
+    <div className="PrivacyContent">
+      <ContentTitle
+        onChange={props.onChange}
+        title="Privacy Policy"
+      ></ContentTitle>
+      <div class='Privacy'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M16 11h5l-9 10-9-10h5v-11h8v11zm1 11h-10v2h10v-2z" />
+        </svg>
+        <p>Click here to download privacy policy</p>
+      </div>
+    </div>
+  );
 }
