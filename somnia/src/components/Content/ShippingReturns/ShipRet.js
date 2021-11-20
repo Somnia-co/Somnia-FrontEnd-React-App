de15f3bbe6ae8ce65f9d43fe7701106a5a5c0dc4
pemix09 <pemix09@onet.pl>
2021-11-20 17:57:48 +0100

    Added shipping page

--- somnia/src/components/Content/ShippingReturns/ShipRet.js
+++ somnia/src/components/Content/ShippingReturns/ShipRet.js
@@ -1,20 +1,20 @@
 import React from "react";
-import './ShipRet.css';
-import ContentTitle from '../ContentTitle/ContentTitle';
+import "./ShipRet.css";
+import ContentTitle from "../ContentTitle/ContentTitle";
 
-
-export default function ShipRet(props){
-    
-
-    
-    return(
-        <div className="ShipRet">
-            <ContentTitle
-                onChange={props.onChange}
-                title='Terms And Conditions'
-            ></ContentTitle>
-            <p>The content of Shipping and returns</p>
-        </div>
-    )
-
+export default function ShipRet(props) {
+  return (
+    <div className="ShipRet">
+      <ContentTitle
+        onChange={props.onChange}
+        title="Terms And Conditions"
+      ></ContentTitle>
+      <div class='Shipping'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M16 11h5l-9 10-9-10h5v-11h8v11zm1 11h-10v2h10v-2z" />
+        </svg>
+        <p>Click here to download Shipping and returns terms</p>
+      </div>
+    </div>
+  );
 }
