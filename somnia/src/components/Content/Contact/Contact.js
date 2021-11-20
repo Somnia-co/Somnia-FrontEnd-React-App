c1755730c035c79ad8beb8a5825334a3e1abe659
pemix09 <pemix09@onet.pl>
2021-11-20 17:43:03 +0100

    Added nice contact us form

--- somnia/src/components/Content/Contact/Contact.js
+++ somnia/src/components/Content/Contact/Contact.js
@@ -1,18 +1,23 @@
 import React from "react";
-import './Contact.css';
-import ContentTitle from '../ContentTitle/ContentTitle';
+import "./Contact.css";
+import ContentTitle from "../ContentTitle/ContentTitle";
 
-export default function Contact(props){
-    return(
-        <div class='ContactUs'>
-            <ContentTitle
-                onChange={props.onChange}
-                title='Contact Us'
-            ></ContentTitle>
-            <label>Message</label>
-            <input class = 'Message'></input>
-            <label>Email address</label>
-            <input class = 'EmailInput'></input>
-        </div>
-    )
+export default function Contact(props) {
+  return (
+    <div class="ContactUs">
+            <ContentTitle onChange={props.onChange} title="Contact Us"></ContentTitle>
+            <div className='SendButton'>
+                Send
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"/></svg>
+            </div>
+      <div class="ContactForm">
+        <label>Your message</label>
+        <input class="Message"></input>
+      </div>
+      <div class="ContactForm">
+        <label>Your email address</label>
+        <input class="EmailInput"></input>
+      </div>
+    </div>
+  );
 }
