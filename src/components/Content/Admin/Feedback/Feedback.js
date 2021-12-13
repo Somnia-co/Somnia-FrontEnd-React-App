adf67e43d5ed1c18e21745c268380695071df98e
DanShepherd27 <jdani20001027@gmail.com>
2021-12-13 02:12:19 +0100

    Basic UI for feedbacks

--- src/components/Content/Admin/Feedback/Feedback.js
+++ src/components/Content/Admin/Feedback/Feedback.js
@@ -4,8 +4,35 @@ import "./Feedback.css";
 function Feedback() {
   return (
     <section id="Feedback">
-      <h1>This is the Feedback page. Listen to your customers!</h1>
-      <h2>Example</h2>
+      <h1>Feedback</h1>
+      <table className="Messages">
+        <thead className="Header">
+          <th>Message</th>
+          <th>Rating</th>
+          <th>From</th>
+          <th>Date</th>
+        </thead>
+        <tbody className="Body">
+          <tr className="Rating">
+            <td>Almost perfect!</td>
+            <td className="Rating rowFlex">4.5/5</td>
+            <td>J. Smith</td>
+            <td>14th March</td>
+          </tr>
+          <tr className="Rating">
+            <td>My favourite brand ever:O</td>
+            <td className="Rating rowFlex">5.0/5</td>
+            <td>B. W. Rich</td>
+            <td>14th March</td>
+          </tr>
+          <tr className="Rating">
+            <td>Just... wow!</td>
+            <td className="Rating rowFlex">5.0/5</td>
+            <td>M. Reeds</td>
+            <td>13th March</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
