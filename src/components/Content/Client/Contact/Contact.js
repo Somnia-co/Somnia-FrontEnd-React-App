d556f7c56e9cfef6e32a055db75d3226bfca7712
Szilvi99 <szilvuski@gmail.com>
2021-12-14 18:23:26 +0100

    correcting the contact page's send button

--- src/components/Content/Client/Contact/Contact.js
+++ src/components/Content/Client/Contact/Contact.js
@@ -7,7 +7,9 @@ export default function Contact(props) {
     <div class="ContactUs">
             <ContentTitle onChange={props.onChange} title="Contact Us"></ContentTitle>
             <div className='SendButton'>
-                Send
+              <form>
+              <input type="submit" value="Send"/><br/>
+              </form>                
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"/></svg>
             </div>
       <div class="ContactForm">
@@ -15,7 +17,7 @@ export default function Contact(props) {
         <input class="Message"></input>
       </div>
       <div class="ContactForm">
-        <label>Your email address</label>
+        <label>Your e-mail address</label>
         <input class="EmailInput"></input>
       </div>
     </div>
