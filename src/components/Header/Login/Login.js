1bb6cec91541d04edc15c33b5898dad20e69a536
DanShepherd27 <jdani20001027@gmail.com>
2021-12-17 11:24:10 +0100

    minor changes with admin and client site buttons

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,26 +1,31 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Login.css";
-import {UseActiveContentContext} from '../../../globalContext/context/ActiveContentContext'
+import { UseActiveContentContext } from "../../../globalContext/context/ActiveContentContext";
+import LoginIcon from "../../../images/Icons/login_white_24dp.svg";
 
 export default function Login(props) {
-  
-  let ActiveContent = UseActiveContentContext()
+  let ActiveContent = UseActiveContentContext();
 
-  useEffect(()=>{
-    
-    if(ActiveContent.value === 'LOGIN' || ActiveContent.value === 'REGISTER')
-    {
+  useEffect(() => {
+    if (ActiveContent.value === "LOGIN" || ActiveContent.value === "REGISTER") {
       const loginIcon = document.getElementById("LoginIcon");
       loginIcon.classList.add("Hidden");
-    }
-    else{
+    } else {
       const loginIcon = document.getElementById("LoginIcon");
       loginIcon.classList.remove("Hidden");
     }
-  },[ActiveContent.value])
+  }, [ActiveContent.value]);
   return (
     <div>
-      <svg
+      <img
+        src={LoginIcon}
+        className="LoginIcon"
+        id="LoginIcon"
+        alt="Login icon"
+        onClick={() => ActiveContent.HandleChange("LOGIN")}
+      />
+
+      {/* <svg
         className="loginIcon"
         id="LoginIcon"
         onClick={() => ActiveContent.HandleChange('LOGIN')}
@@ -30,8 +35,7 @@ export default function Login(props) {
         clipRule="evenodd"
       >
         <path d="M2.598 9h-1.055c1.482-4.638 5.83-8 10.957-8 6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5c-5.127 0-9.475-3.362-10.957-8h1.055c1.443 4.076 5.334 7 9.902 7 5.795 0 10.5-4.705 10.5-10.5s-4.705-10.5-10.5-10.5c-4.568 0-8.459 2.923-9.902 7zm12.228 3l-4.604-3.747.666-.753 6.112 5-6.101 5-.679-.737 4.608-3.763h-14.828v-1h14.826z" />
-      </svg>
-     
+      </svg> */}
     </div>
   );
 }
